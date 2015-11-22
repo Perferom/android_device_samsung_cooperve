9c1a92a1953ed9987e9650c40fc3b07c70ee18b8
JohnHoder <hoder.john@gmail.com>
2015-11-22 01:07:16 +0100

    Initial commit (Cloned)

---
+++ cm.mk
@@ -0,0 +1,21 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit device configuration for cooperve
+$(call inherit-product, device/samsung/cooperve/full_cooperve.mk)
+
+# Inherit some common CM stuff.
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
+$(call inherit-product, vendor/cm/config/mini.mk)
+
+# Overrides
+PRODUCT_NAME := cm_cooperve
+PRODUCT_DEVICE := cooperve
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := GT-S5830i
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_CHARACTERISTICS := phone
+
+PRODUCT_RELEASE_NAME := GalaxyCooperVE
+PRODUCT_VERSION_DEVICE_SPECIFIC := -GT-S5830i
